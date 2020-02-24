5b803194ee3c614a916b6f105c8a96110b89503c
jscruz <jscruzlambert@gmail.com>
2020-02-24 22:04:01 +0000

    Separation of concerns, more go sugar

--- board.go
+++ board.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 )
 
-// RED player
-const RED = 1
-
-// WHITE player
-const WHITE = 0
+// Players
+const (
+	WHITE int = iota
+	RED
+)
 
 // Board backgammon board
 type Board struct {
@@ -23,14 +23,16 @@ func reverse(numbers []int) []int {
 	return newNumbers
 }
 
-func (b *Board) setup() {
+// Setup the backgammon board with the initial position for players
+func (b *Board) Setup() {
 	firstPosition := []int{0, 0, 0, 0, 0, 5, 0, 3, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2}
 	whiteBoard := firstPosition
 	redBoard := reverse(firstPosition)
 	b.board = [][]int{whiteBoard, redBoard}
 }
 
-func (b *Board) move(player int, initialPos int, moves int) error {
+// Move piece from player
+func (b *Board) Move(player int, initialPos int, moves int) error {
 	var adversary = RED
 	moves = moves * -1
 	initialPos--
@@ -44,74 +46,4 @@ func (b *Board) move(player int, initialPos int, moves int) error {
 	b.board[player][initialPos+moves]++
 	b.board[player][initialPos]--
 	return nil
-}
-
-func (b *Board) toString() string {
-	var output string
-	var redPositionUp = make([]int, 12)
-	var whitePositionUp = make([]int, 12)
-	var redPositionDown = make([]int, 12)
-	var whitePositionDown = make([]int, 12)
-	copy(redPositionUp, b.board[1][12:])
-	copy(whitePositionUp, b.board[0][12:])
-	copy(whitePositionDown, b.board[0][:12])
-	copy(redPositionDown, b.board[1][:12])
-	for i := 13; i < 25; i++ {
-		output += fmt.Sprintf("%4v", i)
-	}
-	output += fmt.Sprintf("\n")
-	for i := 12; i > 0; i-- {
-		output += fmt.Sprintf("%4v", "-")
-	}
-	output += fmt.Sprintf("\n")
-
-	for i := 0; i < 7; i++ {
-		for j := 0; j < 12; j++ {
-			if whitePositionUp[j] > 0 || redPositionUp[j] > 0 {
-				if whitePositionUp[j] > 0 {
-					output += fmt.Sprintf("%4v", 0)
-					whitePositionUp[j]--
-				}
-				if redPositionUp[j] > 0 {
-					output += fmt.Sprintf("%4v", "X")
-					redPositionUp[j]--
-				}
-			} else {
-				output += fmt.Sprintf("%4v", " ")
-			}
-		}
-		output += fmt.Sprintf("\n")
-	}
-	output += fmt.Sprintf("   ==============================================\n")
-	var lines [8]string
-	for i := 0; i < 7; i++ {
-		for j := 11; j >= 0; j-- {
-			if whitePositionDown[j] > 0 || redPositionDown[j] > 0 {
-				if whitePositionDown[j] > 0 {
-					lines[i] += fmt.Sprintf("%4v", 0)
-					whitePositionDown[j]--
-				}
-				if redPositionDown[j] > 0 {
-					lines[i] += fmt.Sprintf("%4v", "X")
-					redPositionDown[j]--
-				}
-			} else {
-				lines[i] += fmt.Sprintf("%4v", " ")
-			}
-		}
-		lines[i] += fmt.Sprintf("\n")
-	}
-
-	for i := 6; i >= 0; i-- {
-		output += fmt.Sprintf("%s", lines[i])
-	}
-	for i := 12; i > 0; i-- {
-		output += fmt.Sprintf("%4v", "-")
-	}
-	output += fmt.Sprintf("\n")
-	for i := 12; i > 0; i-- {
-		output += fmt.Sprintf("%4v", i)
-	}
-	output += fmt.Sprintf("\n")
-	return output
 }
--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
 	var board = new(Board)
-	board.setup()
-	fmt.Printf(board.toString())
-	board.move(WHITE, 24, 4)
-	fmt.Printf(board.toString())
-	board.move(WHITE, 24, 4)
-	fmt.Printf(board.toString())
-	board.move(WHITE, 20, 1)
-	fmt.Printf(board.toString())
+	board.Setup()
+	PrintBoard(board)
+	board.Move(WHITE, 24, 4)
+	PrintBoard(board)
+	board.Move(WHITE, 24, 4)
+	PrintBoard(board)
+	board.Move(WHITE, 20, 1)
+	PrintBoard(board)
 }
