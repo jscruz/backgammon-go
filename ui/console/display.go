3b7d0902a1f4facd03a6d10ea3a6ec282a8bfca6
Dan Potepa <dan@danpotepa.co.uk>
2020-02-27 12:16:12 +0000

    using go template

--- ui/console/display.go
+++ ui/console/display.go
@@ -1,23 +1,63 @@
 package console
 
-import "fmt"
-
-const (
-	columns    = 27
-	rows       = 21
-	totalCells = columns * rows
+import (
+	"fmt"
+	"html/template"
+	"os"
 )
 
-type Window [rows * columns]string
+const (
+	BottomBar = 26
+	TopBar = 25
 
-func Draw(w Window) {
+	BottomHome = 0
+	TopHome = 27
+)
 
-	for i := 0; i < rows * columns; i += columns {
+type Model struct{
+	points [28]point
+}
+type point [6]string
 
-		for j := i; j < i + columns; j++ {
-			fmt.Printf("%v ", w[j])
-		}
+func NewModel() *Model {
 
-		fmt.Print("\n")
+	pts := [28]point{}
+
+	m := &Model{pts}
+
+	for pi, _ := range m.points {
+		m.SetPointCount(pi,6,"")
 	}
+
+	return m
+}
+
+func (m *Model) SetPointCount(point, count int, s string) {
+
+	for i := 0; i < count && i < 6; i++ {
+		m.points[point][i] = fmt.Sprintf("%-2v", s)
+	}
+
+	if count > 6 {
+		m.points[point][5] = fmt.Sprintf("%-2v", count - 5)
+	}
+}
+
+func Draw(m *Model)error {
+
+	tpl, err := template.New("board").Funcs(template.FuncMap{
+		"c": func(p, i int, m *Model) string {
+			return m.points[p][i]
+		},
+	}).Parse(outputTemplate)
+	if err != nil {
+		return err
+	}
+
+	err = tpl.Execute(os.Stdout, m)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- ui/console/display_test.go
+++ ui/console/display_test.go
@@ -1,73 +1,21 @@
-package console
+package console_test
 
 import (
-	"strconv"
+	"backgammon_go/ui/console"
 	"testing"
 )
 
-var fullWidthLine = [columns]string{}
-
 func TestDirtyTest(t *testing.T) {
 
-	w := &Window{}
+	m := console.NewModel()
+	m.SetPointCount(12, 4, "x")
 
-	for k := 0; k < totalCells; k++ {
-		w[k] = " "
-	}
+	m.SetPointCount(console.TopBar, 1, "x")
+	m.SetPointCount(console.BottomHome, 20, "x")
+	m.SetPointCount(console.TopHome, 10, "o")
 
-	for i := 0; i < columns; i ++ {
-		fullWidthLine[i] = "-"
-	}
-
-	topRow := 0 + columns
-	midRow := columns * ((rows - 1) / 2 )
-	bottomRow := columns * (rows - 2)
-
-	repeatHorizontal(topRow, "-", w)
-	repeatHorizontal(midRow, "=", w)
-	repeatHorizontal(bottomRow, "-", w)
-
-	repeatVerticleFull(12, "|", w)
-	repeatVerticleFull(13, " ", w)
-	repeatVerticleFull(14, "|", w)
-
-	setPoint(1, 15, "X", w)
-	setPoint(2, 4, "O", w)
-	setPoint(6, 15, "X", w)
-
-	Draw(*w)
-}
-
-func setPoint(point, count int, char string, w *Window) {
-	if count > 6 {
-		for i := 0; i < 5; i++ {
-			c := cells[point][i]
-			w[c] = char
-		}
-
-		c := cells[point][5]
-		w[c] = strconv.Itoa(count)
-	} else {
-		for i := 0; i < count; i++ {
-			c := cells[point][i]
-			w[c] = char
-		}
-	}
-}
-
-func repeatHorizontal(row int, char string, w *Window){
-	for i := 0; i < columns; i += 2 {
-		w[row + i] = char
-	}
-}
-
-func repeatVerticleFull(col int, char string, w *Window) {
-	i := col
-	for {
-		if i > totalCells {
-			break
-		}
-		w[i] = char
-		i += columns
+ 	err := console.Draw(m)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
