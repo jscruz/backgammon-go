9fe28c484908bc143db8d705e3bf09d4eac4fc94
Dan Potepa <dan@danpotepa.co.uk>
2020-03-01 20:08:18 +0000

    added double cubes positions and ui model and template contains die and cube locations

--- ui/console/display_test.go
+++ ui/console/display_test.go
@@ -1,7 +1,9 @@
 package console
 
 import (
+	"backgammon_go/model"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"testing"
 )
 
@@ -13,11 +15,27 @@ func TestUpdatingSamePoint(t *testing.T) {
 	m.SetPointCount(9, 2, "Y")
 
 	// 3rd+ man on the point, it should be back to empty
-	assert.Equal(t, "", m[9][2])
-	assert.Equal(t, "", m[9][3])
-	assert.Equal(t, "", m[9][4])
+	assert.Equal(t, "", m.points[9][2])
+	assert.Equal(t, "", m.points[9][3])
+	assert.Equal(t, "", m.points[9][4])
 
 	//1st and 2nd man should have been changed
-	assert.Equal(t, "Y", m[9][0])
-	assert.Equal(t, "Y", m[9][1])
+	assert.Equal(t, "Y", m.points[9][0])
+	assert.Equal(t, "Y", m.points[9][1])
+}
+
+func TestD(t *testing.T) {
+
+	b := model.Board{}
+	b.Setup()
+
+	m := GenerateViewModelFromBoard(b)
+
+	m.BtmPlyrName = "Dan"
+	m.TopPlyrName = "Jorge"
+
+	err := m.Draw()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
