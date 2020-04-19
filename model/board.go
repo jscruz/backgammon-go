2b985363d4b1507aee83dd5a7461fde10a797e8a
jscruz <jscruzlambert@gmail.com>
2020-04-19 09:08:23 +0100

    Improve test coverage

--- model/board.go
+++ model/board.go
@@ -5,27 +5,44 @@ import (
 	"math/rand"
 )
 
+// Player it could be White: 0 or Red: 1
 type Player int
 
 // Players
 const (
 	White Player = iota
 	Red
-
-	Spaces = 26
-
-	BarPosition = Spaces - 1
-
+	Spaces       = 26
+	BarPosition  = Spaces - 1
 	HomePosition = 0
+	Checkers     = 15
 )
+
+// Model interface for the board model
+type Model interface {
+	NewBoard()
+	Copy(*Model)
+	Move(player Player, initialPos int, moves int) (bool, error)
+	GameFinished() bool
+	GetPossibleMoves(player Player, moves int) []int
+	NextTurn() Player
+	GetCurrentPlayer() Player
+	IsPlayerHome(player Player) bool
+	IsHit(player Player) bool
+}
 
 // Board backgammon board
 type Board struct {
 	Turn  int
 	Board [2][Spaces]int
-	Hit   [2]int
-	Borne [2]int
 	Pips  [2]int
+}
+
+// Copy function to do a deep copy of the board game
+func (b *Board) Copy(c *Board) {
+	b = c
+	b.Board = c.Board
+	b.Pips = c.Pips
 }
 
 func reverse(numbers [Spaces]int) [Spaces]int {
@@ -38,7 +55,7 @@ func reverse(numbers [Spaces]int) [Spaces]int {
 	return newNumbers
 }
 
-// Setup the backgammon board with the initial position for players
+// NewBoard setups the backgammon board with the initial position for players
 func NewBoard() *Board {
 	b := &Board{}
 
@@ -46,34 +63,46 @@ func NewBoard() *Board {
 	var whitePosition = [Spaces]int{0, 0, 0, 0, 0, 0, 5, 0, 3, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0}
 	b.Board = [2][Spaces]int{whitePosition, redPosition}
 	b.Turn = 0
-	b.GetPips()
+	b.UpdatePips()
 	return b
 }
 
-func (b *Board) Move(player Player, initialPos int, moves int) (bool, error) {
-	possibleMoves := b.GetPossibleMoves(player, moves)
-	if len(possibleMoves) < 1 {
-		return false, errors.New("No moves possible")
-	}
-	adversaryBoard := reverse(b.Board[adversary(player)])
-	hasMoved := false
-	if contains(possibleMoves, initialPos) {
+// Move changes the position of checkers in the board given a initial position initialPos and moves we want to take.
+// It will keep it in the Home Position if the number of moves exceeds that position.
+// It checks as well if as an action of moving the other player is hit
+func (b *Board) Move(player Player, initialPos int, moves int) error {
+	if b.isValidMove(player, initialPos, moves) {
 		b.Board[player][initialPos]--
 		if initialPos-moves <= HomePosition {
 			b.Board[player][HomePosition]++
 		} else {
 			b.Board[player][initialPos-moves]++
-			if adversaryBoard[getAdversaryPosition(initialPos)] == 1 {
-				b.Board[adversary(player)][getAdversaryPosition(initialPos)] = 0
-				b.Board[adversary(player)][BarPosition]++
+			opponent := adversary(player)
+			opponentPosition := getAdversaryPosition(initialPos - moves)
+			if b.Board[opponent][opponentPosition] == 1 {
+				b.Board[opponent][opponentPosition]--
+				b.Board[opponent][BarPosition]++
 			}
 		}
-		hasMoved = true
+		b.UpdatePips()
+		return nil
 	}
-	b.GetPips()
-	return hasMoved, nil
+	return errors.New("Illegal move")
 }
 
+func (b *Board) isValidMove(player Player, initialPos int, moves int) bool {
+	if moves > 0 && moves < 7 && initialPos > 0 && initialPos < BarPosition && b.Board[player][initialPos] > 0 {
+		return true
+	}
+	return false
+}
+
+//GameFinished returns true if any of the players has borne 15 checkers
+func (b *Board) GameFinished() bool {
+	return b.Board[White][0] == Checkers || b.Board[Red][0] == Checkers
+}
+
+//GetPossibleMoves returns a list of possible initial position to move the number of player moves
 func (b *Board) GetPossibleMoves(player Player, moves int) []int {
 	currentPlayerBoard := b.Board[player]
 	adversaryBoard := reverse(b.Board[adversary(player)])
@@ -86,61 +115,73 @@ func (b *Board) GetPossibleMoves(player Player, moves int) []int {
 		return possibleMoves
 	}
 
-	for i := 0; i < Spaces; i++ {
-		if i-moves < HomePosition && b.IsPlayerHome(player) && i > HomePosition {
+	for i := 1; i < Spaces; i++ {
+		//Check player is home and moves are equal or minor than HomePosition
+		if b.IsPlayerHome(player) && (i-moves) <= HomePosition && currentPlayerBoard[i] > 0 {
 			possibleMoves = append(possibleMoves, i)
 		}
-		if i-moves > HomePosition && currentPlayerBoard[i] > 0 && adversaryBoard[i-moves] < 2 {
+		if (i-moves) > HomePosition && currentPlayerBoard[i] > 0 && adversaryBoard[i-moves] < 2 {
 			possibleMoves = append(possibleMoves, i)
 		}
 	}
 	return possibleMoves
 }
 
+//RollDie returns a random integer value within 1 to 6
 func RollDie() int {
 	min := 1
 	max := 6
 	return rand.Intn(max-min) + min
 }
 
+//RollDice returns a pair of numbers representing dice in the game.
+func RollDice() []int {
+	var dice []int
+	die1 := RollDie()
+	die2 := RollDie()
+	if die1 == die2 {
+		dice = []int{die1, die2, die1, die2}
+	} else {
+		dice = []int{die1, die2}
+	}
+	return dice
+}
+
+//NextTurn returns which player is next and add a turn to the internal counter
 func (b *Board) NextTurn() Player {
 	b.Turn++
 	return Player(b.Turn % 2)
 }
 
+//GetCurrentPlayer return the current player
 func (b *Board) GetCurrentPlayer() Player {
 	return Player(b.Turn % 2)
 }
 
-func adversary(player Player) Player {
-	switch player {
-	case White:
-		return Red
-	default:
-		return White
-	}
-}
-
+//IsPlayerHome checks is a given player is at home board given that there is no checkers in the rest of the board.
 func (b *Board) IsPlayerHome(player Player) bool {
 	checkers := []int{}
 	sum := 0
-	checkers = b.Board[player][:18]
+	checkers = b.Board[player][7:]
 	for _, num := range checkers {
 		sum += num
 	}
 	return sum == 0
 }
 
+//IsHit checks is a player has any checker in the bar position
 func (b *Board) IsHit(player Player) bool {
 	return b.Board[player][BarPosition] > 0
 }
 
-func (board *Board) GetPips() {
-	board.Pips[White] = CalculatePips(board.Board[White])
-	board.Pips[Red] = CalculatePips(board.Board[Red])
+//UpdatePips calculate pips and assign them to each player pips
+func (b *Board) UpdatePips() {
+	b.Pips[White] = calculatePips(b.Board[White])
+	b.Pips[Red] = calculatePips(b.Board[Red])
 }
 
-func CalculatePips(position [Spaces]int) int {
+//calculatePips calculates pips based on the player positions
+func calculatePips(position [Spaces]int) int {
 	pips := 0
 	for i := 0; i < len(position); i++ {
 		pips += i * position[i]
@@ -151,6 +192,15 @@ func CalculatePips(position [Spaces]int) int {
 func getAdversaryPosition(position int) int {
 	adversary := []int{25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
 	return adversary[position]
+}
+
+func adversary(player Player) Player {
+	switch player {
+	case White:
+		return Red
+	default:
+		return White
+	}
 }
 
 func contains(s []int, e int) bool {
--- model/board_test.go
+++ model/board_test.go
@@ -1,9 +1,10 @@
 package model
 
 import (
-	"fmt"
-	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBoardSetup(t *testing.T) {
@@ -21,9 +22,9 @@ func TestPipsCalculation(t *testing.T) {
 	homePosition := [Spaces]int{}
 	homePosition[HomePosition] = 1
 
-	valuePosition := CalculatePips(position)
-	valuebarPosition := CalculatePips(barPosition)
-	valuehomePosition := CalculatePips(homePosition)
+	valuePosition := calculatePips(position)
+	valuebarPosition := calculatePips(barPosition)
+	valuehomePosition := calculatePips(homePosition)
 
 	assert.Equal(t, 167, valuePosition)
 	assert.Equal(t, 25, valuebarPosition)
@@ -47,11 +48,16 @@ func TestNextTurn(t *testing.T) {
 
 func TestPlayerIsHome(t *testing.T) {
 	board := NewBoard()
-	board.Board[White] = [Spaces]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 14}
+	board.Board[White] = [Spaces]int{12, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	board.NextTurn()
 
 	assert.Equal(t, true, board.IsPlayerHome(White))
 	assert.Equal(t, false, board.IsPlayerHome(Red))
+
+	board.Board[Red] = [Spaces]int{1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	board.Board[White] = [Spaces]int{0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12}
+	assert.Equal(t, true, board.IsPlayerHome(Red))
+	assert.Equal(t, false, board.IsPlayerHome(White))
 }
 
 func TestPossibleMoves(t *testing.T) {
@@ -68,6 +74,19 @@ func TestPossibleMoves(t *testing.T) {
 	assert.True(t, contains(moves, 18))
 	assert.True(t, contains(moves, 19))
 	assert.True(t, contains(moves, 21))
+}
+
+func TestPossibleMovesWhenAtHome(t *testing.T) {
+	board := NewBoard()
+	board.Board[White] = [Spaces]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 2, 2, 5}
+	board.Board[Red] = [Spaces]int{2, 3, 5, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+	moves := board.GetPossibleMoves(Red, 3)
+
+	assert.Equal(t, 3, len(moves))
+	assert.True(t, contains(moves, 1))
+	assert.True(t, contains(moves, 2))
+	assert.True(t, contains(moves, 3))
 }
 
 func TestPossibleMovesWhenInBar(t *testing.T) {
@@ -87,21 +106,137 @@ func TestNoPossibleMovesWhenInBar(t *testing.T) {
 	board.Board[Red] = [Spaces]int{0, 0, 0, 0, 0, 0, 2, 1, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 
 	moves := board.GetPossibleMoves(White, 6)
-	fmt.Printf("%v", moves)
 	assert.Equal(t, 0, len(moves))
 }
 
-func TestAdversaryPosition(t *testing.T){
-	a := [Spaces]int{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25}
+func TestAdversaryPosition(t *testing.T) {
+	a := [Spaces]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
 	b := reverse(a)
-	for i:=0; i < Spaces; i++ {
+	for i := 0; i < Spaces; i++ {
 		assert.Equal(t, b[i], getAdversaryPosition(i))
 	}
 }
 
 func TestContains(t *testing.T) {
-	a := []int{0,1,2,3,4,5,6}
+	a := []int{0, 1, 2, 3, 4, 5, 6}
 
 	assert.True(t, contains(a, 1))
-	assert.False(t, contains(a,19))
+	assert.False(t, contains(a, 19))
+}
+
+func TestCopy(t *testing.T) {
+	a := NewBoard()
+	b := NewBoard()
+
+	b.Board[White] = [Spaces]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 2, 2, 5}
+	b.Board[Red] = [Spaces]int{0, 0, 0, 0, 0, 0, 2, 1, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	b.Pips[White] = 33
+	b.Pips[Red] = 66
+
+	assert.Equal(t, a.Pips[White], 167)
+	assert.Equal(t, a.Pips[Red], 167)
+	assert.Equal(t, a.Board[White][25], 0)
+	assert.Equal(t, a.Board[Red][25], 0)
+
+	a.Copy(b)
+
+	assert.Equal(t, b.Pips[White], 33)
+	assert.Equal(t, b.Pips[Red], 66)
+	assert.Equal(t, b.Board[White][25], 5)
+	assert.Equal(t, b.Board[Red][25], 0)
+
+	a.Board[White][0] = 88
+
+	assert.Equal(t, b.Board[White][0], 0)
+}
+
+func TestDice(t *testing.T) {
+	var numbers []int
+
+	rand.Seed(1)
+	numbers = RollDice()
+	assert.Equal(t, 2, len(numbers))
+	assert.Equal(t, 2, numbers[0])
+	assert.Equal(t, 3, numbers[1])
+
+	// Initialise the seed to force a double dice
+	// I found this value iterating to check RollDice values and increasing the seed
+	// Fortunately, the seed 2 was the one offering a double dice
+	rand.Seed(2)
+	numbers = RollDice()
+	assert.Equal(t, 4, len(numbers))
+	assert.Equal(t, 2, numbers[0])
+	assert.Equal(t, 2, numbers[1])
+	assert.Equal(t, 2, numbers[2])
+	assert.Equal(t, 2, numbers[3])
+}
+
+func TestGameFinished(t *testing.T) {
+	board := NewBoard()
+	assert.False(t, board.GameFinished())
+	board.Board[White] = [Spaces]int{15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	assert.True(t, board.GameFinished())
+}
+
+func TestMoveToEmptyLocation(t *testing.T) {
+	board := NewBoard()
+	board.Board[White] = [Spaces]int{1, 5, 4, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	board.UpdatePips()
+	assert.Equal(t, board.Pips[White], 30)
+	//Try to move from a empty location
+	assert.Error(t, board.Move(White, 20, 3))
+}
+
+func TestMoveAndBear(t *testing.T) {
+	board := NewBoard()
+	board.Board[White] = [Spaces]int{1, 5, 4, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	board.UpdatePips()
+	//Bear a checker, reduce 1 in position 4, increase 1 in position 0 (Home Position), pips recalculated
+	assert.NoError(t, board.Move(White, 4, 5))
+	assert.Equal(t, board.Board[White][0], 2)
+	assert.Equal(t, board.Board[White][4], 1)
+	assert.Equal(t, board.Pips[White], 26)
+}
+
+func TestMoveAndHit(t *testing.T) {
+	board := NewBoard()
+	board.Board[White] = [Spaces]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 1, 2, 2, 0}
+	board.Board[Red] = [Spaces]int{0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0}
+
+	//White moves to a occupied location and hit Red player
+	assert.NoError(t, board.Move(White, 20, 1))
+	assert.Equal(t, 1, board.Board[White][19])
+	assert.Equal(t, 0, board.Board[Red][6])
+	assert.Equal(t, 1, board.Board[Red][BarPosition])
+}
+
+func TestInvalidMove(t *testing.T) {
+	board := NewBoard()
+	board.Board[White] = [Spaces]int{1, 5, 4, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+	assert.True(t, board.isValidMove(White, 4, 2))
+	assert.True(t, board.isValidMove(White, 4, 5))
+	assert.False(t, board.isValidMove(White, 20, 3))
+	assert.False(t, board.isValidMove(White, -4, 2))
+	assert.False(t, board.isValidMove(White, 4, -2))
+	assert.False(t, board.isValidMove(White, 4, 8))
+}
+
+func TestPlayerHasBeenHit(t *testing.T) {
+	board := NewBoard()
+	assert.False(t, board.IsHit(White))
+	board.Board[White] = [Spaces]int{1, 4, 4, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}
+	assert.True(t, board.IsHit(White))
+}
+
+func TestGetCurrentPlayer(t *testing.T) {
+	board := NewBoard()
+	board.Turn = 1
+	assert.Equal(t, Red, board.GetCurrentPlayer())
+
+	board.Turn = 4
+	assert.Equal(t, White, board.GetCurrentPlayer())
+
+	board.Turn = 5
+	assert.Equal(t, Red, board.GetCurrentPlayer())
 }
